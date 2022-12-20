a999aba4be7c69b44f9f8c8340350bfb8017bfbe
EGM17 <garciamoralesedgareduardo@gmail.com>
2022-12-20 13:54:49 -0600

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -169,6 +169,24 @@ window.addEventListener("load", reveal);
 
 
 /**SPLIT SLIDER */
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
