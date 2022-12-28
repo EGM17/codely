5d405fe9759ce62956a72cec3e4564372f6db226
EGM17 <garciamoralesedgareduardo@gmail.com>
2022-12-27 23:24:57 -0600

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -166,3 +166,32 @@ for (let i = 0, len = revealDelayElements.length; i < len; i++) {
 
 window.addEventListener("scroll", reveal);
 window.addEventListener("load", reveal);
+
+
+
+/**
+ * CUSTOM LOGO CAROUSEL
+ */
+
+$(document).ready(function(){
+  $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover:false,
+      responsive: [{
+          breakpoint: 768,
+          setting: {
+              slidesToShow:4
+          }
+      }, {
+          breakpoint: 520,
+          setting: {
+              slidesToShow: 3
+          }
+      }]
+  });
+});
